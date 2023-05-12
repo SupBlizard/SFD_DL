0e1e7246025f18992987012f485b9d816de75bca
SupBlizard <58865670+SupBlizard@users.noreply.github.com>
2023-05-12 05:13:22 +0100

    Format url print

--- sfd_dl.py
+++ sfd_dl.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # Rate limit can be changed to your liking. Keep in mind to not overload the servers
 RATE_LIMIT = 0.1
-TILE_SIZE  = 500
+TILE_SIZE    = 500
 
 def main(username, p):
     try:
@@ -19,16 +19,13 @@ def main(username, p):
     
     layer_url = f"https://img.superfreedraw.com/layers/1/layer_{layer_id}/tiles/0/"
     outfile = f"{username} ({p[0]['x']},{p[0]['y']}) to ({p[1]['x']},{p[1]['y']}).png"
-
     resolution = (abs(p[0]["x"] - p[1]["x"])*TILE_SIZE, abs(p[0]["y"] - p[1]["y"])*TILE_SIZE)
-    selection = (p[1]['x']-p[0]['x'])*(p[1]['y']-p[0]['y'])
-    
+    selection = str((p[1]['x']-p[0]['x'])*(p[1]['y']-p[0]['y']))
 
     # Print information
     print(f"Layer ID:   {layer_id} ({username})")
     print(f"Resolution: {resolution[0]}x{resolution[1]}")
     print(f"Selection:  {selection} tiles \n")
-
 
     # Download tiles to tile directory and get filesize
     download_tiles(layer_url, resolution, selection, p, outfile)
@@ -37,16 +34,15 @@ def main(username, p):
     
 
 def download_tiles(layer_url, resolution, selection, points, outfile):
-    final = Image.new("RGBA", resolution)
-    c = 0
+    final, count = (Image.new("RGBA", resolution), 0)
 
     for y in range(points[0]["y"], points[1]["y"]):
         for x in range(points[0]["x"], points[1]["x"]):
-            print(f"[{c}/{selection}]: ", end="")
+            print(f"[{str(count).zfill(len(selection))}/{selection}]: ", end="")
             try:
                 url = f"{serialise_zoom(x,y,0)}/{x}_{y}/{x}_{y}.png"
                 tile = Image.open(io.BytesIO(urllib.request.urlopen(layer_url + url).read()))
-                print(f"{url}")
+                print(format_tile_url(url))
             except urllib.error.HTTPError as e:
                 if e.code != 404: raise e
                 tile = Image.new("RGBA", (TILE_SIZE, TILE_SIZE), None)
@@ -54,13 +50,18 @@ def download_tiles(layer_url, resolution, selection, points, outfile):
                 
             # Paste new tile onto final image
             final.paste(tile, get_offset(x, y, points))
-            c+=1
+            count += 1
 
             # Ratelimit requests
             time.sleep(RATE_LIMIT)
     
     # Save final render
     final.save(outfile, quality=100)
+
+
+def format_tile_url(url):
+    url = url.split("/")
+    return f"{url[0].rjust(5,' ')}/{url[1].rjust(5,' ')}/{url[2].rjust(9,' ')}"
 
 
 def get_offset(x,y,p):
