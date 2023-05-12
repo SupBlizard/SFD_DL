e87f41cbb4694391899efcc2dcecc27613f786f2
SupBlizard <58865670+SupBlizard@users.noreply.github.com>
2023-05-12 03:24:50 +0100

    Heavy code clean up

--- sfd_dl.py
+++ sfd_dl.py
@@ -4,9 +4,11 @@ import urllib.request, urllib.parse, urllib.error
 from PIL import Image
 
 
-TILE_SIZE  = 500
+# Rate limit can be changed to your liking. Keep in mind to not overload the servers
 RATE_LIMIT = 0.1
 
+# DO NOT CHANGE, THIS IS A CONSTANT
+TILE_SIZE  = 500
 
 def main(username, p):
     try:
@@ -19,39 +21,32 @@ def main(username, p):
         print(f"User layer not found: {e}")
         return 1
     
-    # Download tiles to tile directory
+    layer_url = f"https://img.superfreedraw.com/layers/1/layer_{layer_id}/tiles/0/"
+    resolution = (abs(p[0]["x"] - p[1]["x"])*TILE_SIZE, abs(p[0]["y"] - p[1]["y"])*TILE_SIZE)
     outfile = f'{username} ({p[0]["x"]},{p[0]["y"]}) to ({p[1]["x"]},{p[1]["y"]}).png'
-    download_tiles(layer_id, p, outfile)
-    
 
-# Get layer ID
-def get_layer_from_username(username):
-    with urllib.request.urlopen(f"https://www.superfreedraw.com/api/v1/getLayerByName/{username}") as resp:
-        return json.loads(resp.read())["data"]["id"]
-
-
-# Download tiles between the two points
-def download_tiles(id, p, outfile):
-    layer_API = f"https://img.superfreedraw.com/layers/1/layer_{id}/tiles/0/"
-    res = (abs(p[0]["x"] - p[1]["x"])*TILE_SIZE, abs(p[0]["y"] - p[1]["y"])*TILE_SIZE)
+    # Print layer selection information
     print(f'Resolution: {res[0]}x{res[1]}')
 
-    final = Image.new("RGBA", (res[0], res[1]))
+    # Download tiles to tile directory
+    download_tiles(layer_url, p, resolution, outfile)
+    
+
+def download_tiles(resolution, points, outfile):
+    final = Image.new("RGBA", resolution)
 
     # todo: Loop through extra edge points
-    for y in range(p[0]["y"], p[1]["y"]):
-        for x in range(p[0]["x"], p[1]["x"]):
-            tile = None
+    for y in range(points[0]["y"], points[1]["y"]):
+        for x in range(points[0]["x"], points[1]["x"]):
             try:
-                url = layer_API + f"{int(x < 0)*-1}_{int(y < 0)*-1}/{x}_{y}/{x}_{y}.png"
-                with urllib.request.urlopen(url) as resp:
-                    tile = Image.open(io.BytesIO(resp.read()))
+                url = layer_API + f"{serialise_zoom(x,y,0)}/{x}_{y}/{x}_{y}.png"
+                tile = Image.open(io.BytesIO(urllib.request.urlopen(url).read()))
             except urllib.error.HTTPError as e:
                 if e.code != 404: raise e
                 tile = Image.new("RGBA", (TILE_SIZE, TILE_SIZE), None)
                 
             # Paste new tile onto final image
-            final.paste(tile, (TILE_SIZE*(x-p[0]["x"]), TILE_SIZE*(y-p[0]["y"])))
+            final.paste(tile, get_offset(x, y, points))
 
             # Ratelimit requests
             time.sleep(RATE_LIMIT)
@@ -60,7 +55,20 @@ def download_tiles(id, p, outfile):
     final.save(outfile, quality=100)
 
 
-# Validate user parameters
+def get_offset(x,y,p):
+    return (TILE_SIZE*(x-p[0]["x"]), TILE_SIZE*(y-p[0]["y"]))
+
+
+def serialise_zoom(x,y,z):
+    # todo: add zoom scalar
+    return f"{int(x < 0)*-1}_{int(y < 0)*-1}"
+
+
+def get_layer_from_username(username):
+    with urllib.request.urlopen(f"https://www.superfreedraw.com/api/v1/getLayerByName/{username}") as resp:
+        return json.loads(resp.read())["data"]["id"]
+
+
 def validate_params(argv):
     for i in range(1,5):
         argv[i] = int(argv[i])
